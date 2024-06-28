f5e37ea323bf8ddc59f5dcf4d7d230615b9dd28c
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2024-06-28 22:37:04 +0300

    ProductCard implemented for showing individual Product on page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
+import Product from './customer/components/Product/Product';
 import HomePage from './customer/pages/HomePage/HomePage';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <div>
         <h1 className='text-3xl font-bold text-sky-700'>E-Commerce</h1>
         <br />
-        <HomePage />
+        {/* <HomePage /> */}
+        <Product />
       </div>
       <Footer />
     </div>
